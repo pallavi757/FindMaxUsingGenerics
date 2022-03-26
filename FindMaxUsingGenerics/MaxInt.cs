da375ef68f4f0e4e63108d69d5a09cda0e7a80fd
unknown <kinekarpallavi21@gmail.com>
2022-03-26 22:04:35 +0530

    Find maximum integer

---
+++ FindMaxUsingGenerics/MaxInt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    internal class MaxInt
+    {
+       
+             public int NumberMax(int first_No, int second_No, int third_No)
+              {
+                Console.WriteLine("Three Integer numbers are : num1 = {0} , num2 = {1} , num3 = {2} ", first_No, second_No, third_No);
+
+                if (first_No.CompareTo(second_No) > 0 && first_No.CompareTo(third_No) > 0)
+                {
+                    Console.Write("Maximum integer number is: ");
+                    return first_No;
+                }
+                else if (second_No.CompareTo(third_No) > 0 && second_No.CompareTo(first_No) > 0)
+                {
+                    Console.Write("Maximum Integer Number Is : ");
+                    return second_No;
+                }
+                else if (third_No.CompareTo(first_No) > 0 && third_No.CompareTo(second_No) > 0)
+                {
+                    Console.Write("Maximum Integer Number Is : ");
+                    return third_No;
+                }
+                else
+                {
+                    Console.WriteLine("Numbers are equal");
+                }
+                return 0;
+            }
+        }
+    }
+
---
+++ FindMaxUsingGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindMaxUsingGenerics
+{ 
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaxInt maxint = new MaxInt();
+            Console.WriteLine(maxint.NumberMax(20, 60, 40));
+        }
+    }
+}
